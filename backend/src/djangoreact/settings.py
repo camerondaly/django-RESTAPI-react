da08260e00a50d794f48f2b8c35e4af009541f00
Cameron Daly <camerondaly9@gmail.com>
2020-12-11 09:52:09 -0500

    fixed settings. Small change to keys

--- backend/src/djangoreact/settings.py
+++ backend/src/djangoreact/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
+from config import SECRET_KEY
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '5nb%673_zm1f67#pm-q8#sjlsj*-cjys5q%bedaaf9*4&$o*xk'
+# Moved to hidden file.
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
